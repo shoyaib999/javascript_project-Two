2a94ac57349ab4e82eb015366b1b4dd27d14f691
shoyaib <shoyaibsb999@gmail.com>
2024-05-22 09:10:19 +0400

    completed project

---
+++ script.js
@@ -0,0 +1,14 @@
+let textBox = document.getElementById('textbox');
+
+textBox.addEventListener('input',function(){
+    let text = this.value;
+    let char = text.length;
+    document.getElementById("char").innerHTML = char;
+
+    text = text.trim();
+    let words = text.split(" ");
+    let cleanList = words.filter(function(elm){
+        return elm !=""
+    });
+    document.getElementById("word").innerHTML = cleanList.length;
+});
